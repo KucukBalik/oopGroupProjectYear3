b10ab53f5d1e1460cc986559f8e090e0f24cc08c
Bartlomiej <g00428194@atu.ie>
2025-10-02 19:20:42 +0100

    Added BookController with some starter code

---
+++ books-service/src/main/java/ie/atu/books_service/BookController.java
@@ -0,0 +1,16 @@
+package ie.atu.books_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/book")
+@RestController
+public class BookController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello Book Service";
+    }
+}
