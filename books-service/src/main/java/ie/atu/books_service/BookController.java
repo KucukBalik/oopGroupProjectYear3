a0bf55cf0265c399b494124cb3c8f869bf1a6e9e
Bartlomiej <g00428194@atu.ie>
2025-10-02 19:41:35 +0100

    Change BookController

--- books-service/src/main/java/ie/atu/books_service/BookController.java
+++ books-service/src/main/java/ie/atu/books_service/BookController.java
@@ -1,8 +1,6 @@
 package ie.atu.books_service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/book")
 @RestController
