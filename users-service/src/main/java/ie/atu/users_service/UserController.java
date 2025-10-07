eb5b487be2928e9472432e22c923f802820d9195
G00407095@atu.ie <G00407095@atu.ie>
2025-10-07 13:36:21 +0100

    refactored some code, added two post methods to add one user and to add multiple users

---
+++ users-service/src/main/java/ie/atu/users_service/UserController.java
@@ -0,0 +1,35 @@
+package ie.atu.users_service;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/Users")
+@RestController
+public class UserController {
+    List<User> userList = new ArrayList<>();
+
+    // basic hello endpoint to test swagger
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello user";
+    }
+
+    // Post method to create a single user
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User myUser)
+    {
+        userList.add(myUser);
+        return myUser;
+    }
+
+    // Post method to add multiple users
+    @PostMapping
+    public List<User> addUsers(@RequestBody List<User> users){
+        userList.addAll(users);
+        return userList;
+    }
+}
