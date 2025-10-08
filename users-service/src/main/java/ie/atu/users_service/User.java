697d2b39ef29c3ae736ef2a9d83f05660af8d38e
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-08 15:23:07 +0100

    Added Validation for Users

--- users-service/src/main/java/ie/atu/users_service/User.java
+++ users-service/src/main/java/ie/atu/users_service/User.java
@@ -1,12 +1,18 @@
 package ie.atu.users_service;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 30)
     private String email;
+    @NotBlank
+    @Size(min = 8, max = 30)
     private String password;
 
 }
