0d52c643c909356556a9dcb2121bd069c6022bc3
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-10-08 15:08:15 +0100

    Added basic validation using @Valid checks for Blank entry, size and a custom error message

--- books-service/src/main/java/ie/atu/books_service/Book.java
+++ books-service/src/main/java/ie/atu/books_service/Book.java
@@ -1,13 +1,22 @@
 package ie.atu.books_service;
 
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Book {
+    @NotBlank
+    @Size(min = 1, max = 100, message ="Invalid Entry")
     private String name;
+    @NotBlank
+    @Size(min = 1, max = 100, message ="Invalid Entry")
     private String author;
+    @NotBlank
+    @Size(min = 1, max = 100, message ="Invalid Entry")
     private String publisher;
 }
