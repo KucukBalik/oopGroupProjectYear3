a3a66e72a0d825ea5016bab4c97ffb240a043ba3
G00407095@atu.ie <G00407095@atu.ie>
2025-10-15 18:06:36 +0100

    created seperation of concerns on user service. TODO: finish find by id method and add implementation

--- users-service/src/main/java/ie/atu/users_service/service/UserService.java
+++ users-service/src/main/java/ie/atu/users_service/service/UserService.java
@@ -1,4 +1,36 @@
 package ie.atu.users_service.service;
 
+import ie.atu.users_service.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final List<User> userList = new ArrayList<>();
+
+    // Defensive Copy of User List
+    public List<User> getUserList(){
+        return new ArrayList<>(userList);
+    }
+
+    public Optional<User> getUserByID(String id){
+        for (User user : userList){
+            if(user.getUserID().equals(id)){
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public User createUser(User user){
+        // add find by ID method once added to controller class
+        userList.add(user);
+        return user;
+    }
+
+
 }
