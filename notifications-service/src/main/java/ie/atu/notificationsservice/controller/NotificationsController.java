30b919be407e3b2bc6305a8d92f4e809d98a1763
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-11-26 15:36:39 +0000

    Start Notifications Service

--- notifications-service/src/main/java/ie/atu/notificationsservice/controller/NotificationsController.java
+++ notifications-service/src/main/java/ie/atu/notificationsservice/controller/NotificationsController.java
@@ -1,6 +1,7 @@
 package ie.atu.notificationsservice.controller;
 
 import ie.atu.notificationsservice.service.NotificationsService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,12 @@ public class NotificationsController {
 
 
     @GetMapping(" /ReturnAllUsers")
+
+    public ResponseEntity<String> ReturnAllUsers() {
+        return ResponseEntity.ok(notificationsService.ReturnAllUsers());
+    }
     // Rethink endpoints needed
+
 }
 //**********************************************************************
 /*
--- notifications-service/src/main/java/ie/atu/notificationsservice/service/NotificationsService.java
+++ notifications-service/src/main/java/ie/atu/notificationsservice/service/NotificationsService.java
@@ -1,4 +1,13 @@
 package ie.atu.notificationsservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class NotificationsService {
+
+
+    public String ReturnAllUsers() {
+        return "Returning all users";
+    }
 }
